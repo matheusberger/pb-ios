014ad0de3f3c847f74ebd19260c8aa54115a6f06
Matheus Migge <matheusmigge@gmail.com>
2023-08-22 20:58:19 -0300

    created xcode project

---
+++ PedalBoard/PedalBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  PedalBoardApp.swift
+//  PedalBoard
+//
+//  Created by Migge on 22/08/23.
+//
+
+import SwiftUI
+
+@main
+struct PedalBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
