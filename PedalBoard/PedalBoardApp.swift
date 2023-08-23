58aa276cb3c6e38de357270c103627d822f2340c
Matheus Migge <matheusmigge@gmail.com>
2023-08-22 21:34:57 -0300

    created pedalcore target

--- PedalBoard/PedalBoardApp.swift
+++ PedalBoard/PedalBoardApp.swift
@@ -6,12 +6,13 @@
 //
 
 import SwiftUI
+import PedalCore
 
 @main
 struct PedalBoardApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
         }
     }
 }
---
+++ PedalCore/PedalCore.h
@@ -0,0 +1,18 @@
+//
+//  PedalCore.h
+//  PedalCore
+//
+//  Created by Migge on 22/08/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PedalCore.
+FOUNDATION_EXPORT double PedalCoreVersionNumber;
+
+//! Project version string for PedalCore.
+FOUNDATION_EXPORT const unsigned char PedalCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PedalCore/PublicHeader.h>
+
+
