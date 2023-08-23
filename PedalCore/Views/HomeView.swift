29455b69703688b15854125895257b3f36854609
Matheus Migge <matheusmigge@gmail.com>
2023-08-22 23:25:05 -0300

    getting pedalName with textField

--- PedalCore/Views/HomeView.swift
+++ PedalCore/Views/HomeView.swift
@@ -9,17 +9,22 @@ import SwiftUI
 
 public struct HomeView: View {
     @ObservedObject var viewModel: HomeViewModel
+    @State var pedalName: String = ""
     
     public init() {
         viewModel = HomeViewModel()
     }
+    
     public var body: some View {
-        Button {
-            viewModel.addPedal(name: "Test", brand: "Cool Brand", knobs: [:])
-        } label: {
-            Text("Create pedal")
+        VStack {
+            TextField("Pedal name:", text: $pedalName, prompt: Text("Name your pedal here"))
+            Button {
+                viewModel.addPedal(name: pedalName, brand: "Cool Brand", knobs: [:])
+                pedalName = ""
+            } label: {
+                Text("Create pedal")
+            }
         }
-
     }
 }
 
