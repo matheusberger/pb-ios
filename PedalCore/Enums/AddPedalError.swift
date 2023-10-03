cd5b4f7447f11933afc61d963228f2cfb1cc885f
Lucas Migge <53379393+miggelucas@users.noreply.github.com>
2023-10-02 23:20:09 -0300

    feature(checkKnobName): alert when missing pedal info

---
+++ PedalCore/Enums/AddPedalError.swift
@@ -0,0 +1,13 @@
+//
+//  AddPedalError.swift
+//  PedalCore
+//
+//  Created by Lucas Migge on 02/10/23.
+//
+
+import Foundation
+
+
+enum AddPedalError: Error {
+    case missingName, missingBrand, missingKnobs
+}
--- PedalCore/ViewModels/HomeViewModel.swift
+++ PedalCore/ViewModels/HomeViewModel.swift
@@ -9,8 +9,29 @@ import Foundation
 
 class HomeViewModel: ObservableObject {
     @Published var pedals: [Pedal] = []
+    @Published var isShowingSheet: Bool = false
 
-    func addPedal(name: String, brand: String, knobNames: [String]) {
+
+    func addIconPressed() {
+        isShowingSheet = true
+    }
+}
+
+extension HomeViewModel: AddPedalDelegate {
+    func addPedalPressed(name: String, brand: String, knobNames: [String]) throws {
+        if name.isEmpty {
+            throw AddPedalError.missingName
+        }
+        
+        if brand.isEmpty {
+            throw AddPedalError.missingBrand
+        }
+        
+        if knobNames.isEmpty {
+            throw AddPedalError.missingKnobs
+        }
+        
+        
         var knobs: [Knob] {
             knobNames.map { name in
                 Knob(name: name)
@@ -19,13 +40,12 @@ class HomeViewModel: ObservableObject {
         
         let newPedal = Pedal(name: name, brand: brand, knobs: knobs)
         pedals.append(newPedal)
-    }
-}
-
-extension HomeViewModel: AddPedalDelegate {
-    func addPedalPressed(name: String, brand: String, knobNames: [String]) {
+        isShowingSheet = false
+        
         
     }
+    
+    
     
     
 }
