15703cf9df99beb5412b56be6ce643eca78c76ee
Matheus Berger <mcoelhoberger@apple.com>
2023-12-08 22:59:40 -0300

    Created PedalProvider using JsonDataService

--- PedalCore/Providers/PedalProvider.swift
+++ PedalCore/Providers/PedalProvider.swift
@@ -6,3 +6,45 @@
 //
 
 import Foundation
+
+class PedalProvider {
+    static let shared = PedalProvider()
+    private let persistenceService: any PersistenceProtocol<Pedal>
+    
+    private var pedals: [Pedal] = [] {
+        didSet {
+            persistenceService.update(pedals)
+        }
+    }
+    
+    init() {
+        self.persistenceService = JsonDataService<Pedal>(filePath: "")
+        do {
+            try persistenceService.load { data in
+                self.pedals = data
+            }
+        } catch {
+            print(error)
+        }
+    }
+    
+    func addPedal(_ pedal: Pedal) {
+        pedals.append(pedal)
+    }
+    
+    func updatePedal(_ pedal: Pedal) {
+        pedals = pedals.map {
+            $0.id == pedal.id ? pedal : $0
+        }
+    }
+    
+    func delete(_ pedal: Pedal) {
+        pedals = pedals.filter {
+            $0.id != pedal.id
+        }
+    }
+    
+    enum PedalProviderErrors: Error {
+        case loadingError(String)
+    }
+}
