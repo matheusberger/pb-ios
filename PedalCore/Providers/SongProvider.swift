a5a11655c524c92796467b7fd488f3fad8db31aa
Matheus Berger <mcoelhoberger@apple.com>
2023-12-08 22:59:10 -0300

    Created SongProvider using JsonDataService

--- PedalCore/Providers/SongProvider.swift
+++ PedalCore/Providers/SongProvider.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+class SongProvider {
+    static let shared = SongProvider()
+    private let persistenceService: any PersistenceProtocol<Song>
+    
+    private(set) var songs: [Song] = []
+    
+    init() {
+        self.persistenceService = JsonDataService<Song>(filePath: "")
+        do {
+            try persistenceService.load { data in
+                self.songs = data
+            }
+        } catch {
+            print(error)
+        }
+    }
+    
+    func update(_ songs: [Song]) {
+        self.songs = songs
+        persistenceService.update(songs)
+    }
+    
+    enum PedalProviderErrors: Error {
+        case loadingError(String)
+    }
+}
