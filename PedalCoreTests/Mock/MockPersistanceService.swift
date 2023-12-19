21bbc2ffc4e24102a4f5f415cca595d96fa3a72d
Matheus Berger <mcoelhoberger@apple.com>
2023-12-19 18:20:31 -0300

    Created MockPersistanceService in PedalCoreTests

--- PedalCoreTests/Mock/MockPersistanceService.swift
+++ PedalCoreTests/Mock/MockPersistanceService.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+import PedalCore
+
+class MockPersistanceService<T>: PersistenceProtocol where T: Hashable {
+    
+    private var shouldThrowSaving: Bool
+    private var shouldThrowLoading: Bool
+    
+    init(shouldThrowSaving: Bool, shouldThrowLoading: Bool) {
+        self.shouldThrowSaving = shouldThrowSaving
+        self.shouldThrowLoading = shouldThrowLoading
+    }
+    
+    func save(_: [T]) throws {
+        if shouldThrowSaving {
+            return
+        }
+        
+        return
+    }
+    
+    func load(_ onLoad: ([T]) -> Void) throws {
+        if shouldThrowLoading {
+            
+        }
+        
+        onLoad([])
+    }
+    
+    enum MockedError: Error {
+        case failedSave(String)
+        case failedLoad(String)
+    }
+}
