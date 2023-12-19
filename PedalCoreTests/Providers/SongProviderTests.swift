7a9493ed1b4ac6b699fb5880bd62e50b85414c26
Matheus Berger <mcoelhoberger@apple.com>
2023-12-19 16:47:00 -0300

    created basic SongProviderTests

--- PedalCoreTests/Providers/SongProviderTests.swift
+++ PedalCoreTests/Providers/SongProviderTests.swift
@@ -9,23 +9,25 @@ import XCTest
 @testable import PedalCore
 
 final class SongProviderTests: XCTestCase {
+    private var provider: SongProvider?
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        provider = SongProvider()
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testUpdate() throws {
+        let expectedSongs = Song.getSample()
+        try provider?.update(expectedSongs)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func testLoad() throws {
+        let expectedSongs = Song.getSample()
+        try provider?.update(expectedSongs)
+        try provider?.load{ songs in
+            XCTAssert(songs.count == expectedSongs.count)
+        }
     }
-
+    
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
         self.measure {
