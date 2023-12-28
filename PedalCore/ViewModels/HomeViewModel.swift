a922f9d017ab3e053f28e1b83ffa74ac0e421407
Matheus Berger <mcoelhoberger@apple.com>
2023-12-28 19:06:21 -0300

    Added persistence code to HomeViewModel without handling errors

--- PedalCore/ViewModels/HomeViewModel.swift
+++ PedalCore/ViewModels/HomeViewModel.swift
@@ -13,17 +13,29 @@ class HomeViewModel: ObservableObject {
         case empty, content
     }
     
-    @Published var allPedals: [Pedal] = []
+    @Published var allPedals: [Pedal] {
+        didSet {
+            do {
+                try provider.update(allPedals)
+            } catch {
+                print(error)
+//                isShowingAlert = true
+//                alert = Alert(title: Text("Saving error"),
+//                              message: Text(error.localizedDescription),
+//                              dismissButton: .default(Text("OK"), action: {
+//                    self.isShowingAlert = false
+//                    self.alert = nil
+//                }))
+            }
+        }
+    }
+    
     @Published var isShowingSheet: Bool = false
     @Published var searchText: String = ""
     
     var editPedal: Pedal?
     
     private var provider: LocalDataProvider<Pedal>
-    
-    init(provider: LocalDataProvider<Pedal>) {
-        self.provider = provider
-    }
     
     var state: State {
         if allPedals.isEmpty {
@@ -40,6 +52,30 @@ class HomeViewModel: ObservableObject {
             return allPedals.filter { pedal in
                 pedal.name.localizedCaseInsensitiveContains(searchText) || pedal.brand.localizedCaseInsensitiveContains(searchText)
             }
+        }
+    }
+    
+    init(provider: LocalDataProvider<Pedal>) {
+        self.provider = provider
+        self.allPedals = []
+        
+        load()
+    }
+    
+    private func load() {
+        do {
+            try provider.load { pedals in
+                allPedals = pedals
+            }
+        } catch {
+            print(error)
+//            isShowingAlert = true
+//            alert = Alert(title: Text("Loading error"),
+//                          message: Text(error.localizedDescription),
+//                          dismissButton: .default(Text("OK"), action: {
+//                self.isShowingAlert = false
+//                self.alert = nil
+//            }))
         }
     }
 
